0291804315e8ba092c0716fbd15e0bcb437bdd92
Vasamshetti sai <139035044+vsai0111@users.noreply.github.com>
2023-12-15 22:31:06 +0530

    update

--- script.js
+++ script.js
@@ -1,39 +1,65 @@
-// scripts.js
-document.addEventListener('DOMContentLoaded', function() {
-  const loginForms = document.getElementById('loginForms');
-  const createAccountForm = document.getElementById('createAccountForm');
-  const createAccountBtn = document.getElementById('createAccountBtn');
-
-  // Function to handle account creation
-  function createAccount(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    const newUsername = document.getElementById('newUsername').value;
-    const newPassword = document.getElementById('newPassword').value;
-    const confirmPassword = document.getElementById('confirmPassword').value;
-
-    if (newPassword !== confirmPassword) {
-      alert('Passwords do not match. Please try again.');
-    } else if (localStorage.getItem(newUsername)) {
-      alert('Username already exists. Please choose a different username.');
-    } else {
-      localStorage.setItem(newUsername, JSON.stringify({ email, newPassword }));
-      alert('Account created successfully!');
-      document.getElementById('email').value = '';
-      document.getElementById('newUsername').value = '';
-      document.getElementById('newPassword').value = '';
-      document.getElementById('confirmPassword').value = '';
-      toggleForms(); // Hide create account form after successful creation
+document.addEventListener('DOMContentLoaded', function () {
+  window.addEventListener('message', function (event) {
+    if (event.source === window && event.data.type === 'FROM_EXTENSION') {
+      const receivedData = event.data.data;
+      if (Array.isArray(receivedData)) {
+        console.log('Received bookmarks from extension:', receivedData);
+        // Process received data (bookmarks)
+        // Update the UI to display the received bookmarks here
+      }
     }
-  }
-
-  // Event listener for Create Account form submission
-  createAccountForm.addEventListener('submit', createAccount);
-
-  // Event listener for Create Account button click
-  createAccountBtn.addEventListener('click', function() {
-    toggleForms();
   });
 
-  // Other functions remain the same
+  // Your existing website code...
+  const bookmarksList = document.getElementById('bookmarks-list');
+  const addBookmarkForm = document.getElementById('addBookmarkForm');
+
+  function displayBookmarks() {
+    const bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    bookmarksList.innerHTML = '';
+    bookmarks.forEach(bookmark => {
+      const bookmarkElement = createBookmarkElement(bookmark);
+      bookmarksList.appendChild(bookmarkElement);
+    });
+  }
+
+  function createBookmarkElement(bookmark) {
+    const bookmarkElement = document.createElement('div');
+    bookmarkElement.classList.add('bookmark');
+    bookmarkElement.innerHTML = `
+      <h3>${bookmark.title}</h3>
+      <p><a href="${bookmark.url}" target="_blank">${bookmark.url}</a></p>
+      <div class="actions">
+        <button onclick="deleteBookmark('${bookmark.title}')">Delete</button>
+      </div>
+    `;
+    return bookmarkElement;
+  }
+
+  function deleteBookmark(title) {
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    bookmarks = bookmarks.filter(bookmark => bookmark.title !== title);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    displayBookmarks();
+  }
+
+  addBookmarkForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const bookmarkTitle = document.getElementById('bookmarkTitle').value;
+    const bookmarkURL = document.getElementById('bookmarkURL').value;
+
+    const newBookmark = {
+      title: bookmarkTitle,
+      url: bookmarkURL
+    };
+
+    let bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+    bookmarks.push(newBookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    displayBookmarks();
+    addBookmarkForm.reset();
+  });
+
+  // Display bookmarks when the page loads
+  displayBookmarks();
 });
